a087f66dcdcac4cfc8bef0a31f2e4470fb1a4175
Marius Weidner <weidner@cs.uni-freiburg.de>
2021-10-14 14:19:08 +0200

    Update instuctions & fix Git URLs

--- gen-salt.sh
+++ gen-salt.sh
@@ -3,4 +3,4 @@
 # usage: ./gen-salt <LENGTH>
 # example: ./gen-salt 24 outputs 3rs$ThWmhpqjO(%7l_QAqbah
 
-tr -dc 'A-Za-z0-9!$%&()[]_{}~' </dev/urandom | head -c "$1"; echo
+tr -dc 'A-Za-z0-9!$&()[]_{}~' </dev/urandom | head -c "$1"; echo
